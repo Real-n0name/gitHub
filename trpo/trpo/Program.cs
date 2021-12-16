c7a59e0e21bf58fd528fb4cb7a9c0ab7f57fa9ca
egork <karachevegor26@gmail.com>
2021-12-16 22:25:19 +0300

    first lab

---
+++ trpo/trpo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace trpo
+{
+    class Program
+    {
+        class A
+        {
+
+        }
+
+        class B : A
+        {
+            public object a;
+            public object b;
+        }
+
+        class C : B
+        {
+            public static void Main()
+            {
+                A a1 = new A();
+                B b2 = new B();
+                B b3 = new B();
+                B b4 = new B();
+                C c5 = new C();
+
+                b2.a = a1;
+                b3.a = a1;
+                b4.a = b2;
+                c5.a = b4;
+                c5.b = b3;
+            }
+        }
+    }
+}
