faf17a9e1a5aad11761457b833e0bf6e6e202374
egork <karachevegor26@gmail.com>
2021-12-16 22:31:35 +0300

    third lab

---
+++ trpo/trpo/Core/LogInterface .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EgorKarachev
+{
+    interface LogInterface
+    {
+        public LogInterface log(string str);
+        public LogInterface write();
+    }
+}
